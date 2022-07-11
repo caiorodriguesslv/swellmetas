50c113a079c6ea6392517198c909f58dddb354d8
unknown <caiorodriguesmll@gmail.com>
2022-07-11 11:42:35 -0300

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import iconredbtn from '../../assets/img/redbtn.svg'
+
+import './styles.css'
+function NotificationButton() {
+    return (
+        <div className="swell-red-btn">
+            <img src={iconredbtn} alt="Notificar"/>
+        </div>
+    )
+}
+
+export default NotificationButton
