e8b93e5be32662f267c3982119dc3025a8bf3c6d
unknown <caiorodriguesmll@gmail.com>
2022-07-11 11:05:18 -0300

    Primeira versao

---
+++ backend/src/main/java/com/caiorodrigues/swellmetas/SwellmetasApplication.java
@@ -0,0 +1,13 @@
+package com.caiorodrigues.swellmetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwellmetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwellmetasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/caiorodrigues/swellmetas/SwellmetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiorodrigues.swellmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwellmetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
